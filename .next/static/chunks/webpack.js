f62b6d192aa877706d817525e4faf3d65c92f439
Melih <ahmetmelih59@gmail.com>
2023-05-17 16:03:30 +0300

    last commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "942f46a61d1c09f3"; }
+/******/ 		__webpack_require__.h = function() { return "0d43924725287a79"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
